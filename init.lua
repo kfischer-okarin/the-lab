0aa91ca303c33fcb0a22eda2920b12d164f0d140
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-07-31 15:35:39 +0900

    Add init.lua

---
+++ init.lua
@@ -0,0 +1,75 @@
+hs.loadSpoon('MenubarCountdown')
+hs.loadSpoon('Slack')
+
+REMOTE_CHANNEL = '1_remote'
+
+WORK_START_MESSAGE = '・おはようございます'
+WORK_FINISH_MESSAGE = '・終了'
+LUNCHBREAK_START_MESSAGE = '・昼休憩します :bento:'
+LUNCHBREAK_FINISH_MESSAGE = '・再開'
+LUNCHBREAK_STATUS_MESSAGE = '昼休憩中'
+LUNCHBREAK_STATUS_EMOJI = ':bento:'
+
+
+function openMenu()
+  return {
+    {
+      title = "Start Work",
+      fn = startWork
+    },
+    {
+      title = "Start Lunchbreak",
+      fn = startLunchbreak
+    },
+    {
+      title = "Finish Work",
+      fn = finishWork
+    }
+  }
+end
+
+function startWork()
+  sendToRemoteChannel(WORK_START_MESSAGE)
+end
+
+function startLunchbreak()
+  spoon.Slack.toggleAway()
+  spoon.Slack.setStatus(LUNCHBREAK_STATUS_MESSAGE, LUNCHBREAK_STATUS_EMOJI)
+  sendToRemoteChannel(LUNCHBREAK_START_MESSAGE)
+  startLunchbreakCountdown()
+end
+
+function finishLunchbreak()
+  lunchbreakCountdown:stop()
+  spoon.Slack.toggleAway()
+  spoon.Slack.clearStatus()
+  sendToRemoteChannel(LUNCHBREAK_FINISH_MESSAGE)
+end
+
+lunchbreakCountdown = nil
+
+function startLunchbreakCountdown()
+  lunchbreakCountdown = spoon.MenubarCountdown.new(
+    'Lunchbreak',
+    os.time() + 60 * 60,
+    {
+      onClick = finishLunchbreak
+    }
+  )
+  lunchbreakCountdown:start()
+end
+
+function finishWork()
+  sendToRemoteChannel(WORK_FINISH_MESSAGE)
+end
+
+function sendToRemoteChannel(message)
+  spoon.Slack.sendMessageToChannel(REMOTE_CHANNEL, message)
+end
+
+menu = hs.menubar.new()
+
+if menu then
+  menu:setTitle('Shortcuts')
+  menu:setMenu(openMenu)
+end
