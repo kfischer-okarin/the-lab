1897772eb75a0c691ec1761defecbdefe8d09380
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-01-14 22:39:10 +0900

    Reorder tests & fix final comma

--- 2025-01-06-vm/vm/assembler_tests.rb
+++ 2025-01-06-vm/vm/assembler_tests.rb
@@ -9,10 +9,6 @@ describe VM::Assembler do
 
   [
     ['; This is a comment line', []],
-    ['ADD R2, R0, R1;',   ['0001010000000001']],
-    ['ADD R3, R4, #1;',   ['0001011100100001']],
-    ['ADD R6, R7, #-12;', ['0001110111110100']],
-    ['LDI R1, Data;',     ['1010001111101111'], { 'data' => 0x2FF0 }],
     ['BRp Done;',         ['0000001000000001'], { 'done' => 0x3002 }],
     ['BRz Done;',         ['0000010000000001'], { 'done' => 0x3002 }],
     ['BRn Done;',         ['0000100000000001'], { 'done' => 0x3002 }],
@@ -21,6 +17,10 @@ describe VM::Assembler do
     ['BRnp Done;',        ['0000101000000001'], { 'done' => 0x3002 }],
     ['BRnzp Done;',       ['0000111000000001'], { 'done' => 0x3002 }],
     ['BR Done;',          ['0000111000000001'], { 'done' => 0x3002 }],
+    ['ADD R2, R0, R1;',   ['0001010000000001']],
+    ['ADD R3, R4, #1;',   ['0001011100100001']],
+    ['ADD R6, R7, #-12;', ['0001110111110100']],
+    ['LDI R1, Data;',     ['1010001111101111'], { 'data' => 0x2FF0 }]
   ].each do |line, expected_machine_code_instructions, labels = {}|
     it "can assemble '#{line}'" do
       assembler = VM::Assembler.new(start_address: 0x3000, labels: labels)
@@ -90,7 +90,7 @@ describe VM::Assembler do
     ['ADD R2, R0, R1, R3;', 'Wrong number of operands'],
     ['ADD #1, R0, R1;', 'Expected register as operand 1'],
     ['ADD R0, R1, #16;', 'Immediate value #16 out of range (-16..15)'],
-    ['LDI R2, #256;', 'Expected label as operand 2'],
+    ['LDI R2, #256;', 'Expected label as operand 2']
   ].each do |instruction, expected_message|
     it "raises an error for invalid instruction '#{instruction}'" do
       assembler = VM::Assembler.new(start_address: 0x3000)
